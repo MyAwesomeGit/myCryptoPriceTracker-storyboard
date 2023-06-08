b949d602bd0d7203f44c20794462e2f4ea0f733e
Vasily Shuklin <b88bb@yandex.ru>
2023-06-08 20:54:41 +0500

    Add project files
    Signed-off-by: Vasily Shuklin <b88bb@yandex.ru>

---
+++ myCryptoPriceTracker/ViewController.swift
@@ -0,0 +1,80 @@
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var crpCcy: [String] = []
+    
+    var ccy: [String] = []
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return crpCcy.count
+        } else {
+            return ccy.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return crpCcy[row]
+        } else {
+            return ccy[row]
+        }
+    }
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    @IBOutlet weak var price: UILabel!
+    
+    @IBAction func refreshButtonTapped(_ sender: Any) {
+        getPrice(crpCcy: crpCcy[picker.selectedRow(inComponent: 0)], ccy: ccy[picker.selectedRow(inComponent: 1)])
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        crpCcy = ["BTC", "DOGE", "ETH", "PEPE", "SNEK" ,"SHIB"]
+        ccy = ["RUR", "AED", "EUR", "HKD", "JPY", "TRY", "USD"]
+        
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        getPrice(crpCcy: crpCcy[picker.selectedRow(inComponent: 0)], ccy: ccy[picker.selectedRow(inComponent: 1)])
+        
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        getPrice(crpCcy: crpCcy[picker.selectedRow(inComponent: 0)], ccy: ccy[picker.selectedRow(inComponent: 1)])
+    }
+    
+    
+    func getPrice (crpCcy: String, ccy: String) {
+        if let url = URL(string: "https://min-api.cryptocompare.com/data/price?fsym=\(crpCcy)&tsyms=\(ccy)") {
+            URLSession.shared.dataTask(with: url) {(data, response, error) in
+                if let data = data {
+                    if let json = try?JSONSerialization.jsonObject(with: data, options: []) as? [String:Double] {
+                        DispatchQueue.main.async {
+                            if let price = json[ccy] {
+                                let formatter = NumberFormatter()
+                                formatter.currencyCode = ccy
+                                formatter.numberStyle = .currency
+                                let formattedPrice = formatter.string(from: NSNumber(value: price))
+                                self.price.text = formattedPrice
+                            }
+                        }
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.price.text = "An error occurred. Press Refresh"
+                    }
+                }
+            }.resume()
+        }
+    }
+    
+}
